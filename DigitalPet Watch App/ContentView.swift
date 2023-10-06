aedbe0bdc6f5bc6a90815d42ac0a9fecf9103c88
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-10-06 15:35:46 -0400

    Added main file

---
+++ DigitalPet Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DigitalPet Watch App
+//
+//  Created by Joaquin Castrillon on 10/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DigitalPet Watch App/DigitalPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalPetApp.swift
+//  DigitalPet Watch App
+//
+//  Created by Joaquin Castrillon on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DigitalPet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
