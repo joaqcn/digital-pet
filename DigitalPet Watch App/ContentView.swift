5fcc89726b44893bcda9ef22c45a61b348c937b0
Joaquin Castrillon <62633394+joaqcn@users.noreply.github.com>
2023-10-06 15:50:17 -0400

    Defined structure of the app

--- DigitalPet Watch App/ContentView.swift
+++ DigitalPet Watch App/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack
+        {
+           //  PET
+            
+            //Here goes the pet selected
+            Text("PET")
+                .font(.largeTitle)
+                .border(Color.red)
+                .padding(.bottom,20)
+            
+            //Spacer to make sprite going up
+            Spacer()
+        // PET STATS
+            VStack
+            {
+                HStack
+                {
+                    Text("Stat 1")
+                    Text("Here go the level bar").font(.footnote)
+                }
+                HStack
+                {
+                    Text("Stat 2")
+                    Text("Here go the level bar").font(.footnote)
+                    
+                }
+                HStack
+                {
+                    Text("Stat 3")
+                    Text("Here go the level bar").font(.footnote)
+                    
+                }
+                
+            } // VStack
+            
+            
+            
+            
+            
+        } // VStack
+        
+        
     }
 }
 
